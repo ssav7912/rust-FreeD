7514a4e08f94aea6ff6f5d8099e322ca1bec3e05
ssav7912 <ssav7912@uni.sydney.edu.au>
2022-12-05 14:45:15 +1100

    Added struct definitions for all protocol command types

--- src/freed.rs
+++ src/freed.rs
@@ -1,18 +1,117 @@
+use crate::common::*;
 
 use std::vec;
 
-use ux::u24;
 use ux::i24;
+use ux::u24;
 
-const ALL_CAMERAS: u8 = 0xFF;
 
+#[derive(Copy, Clone)]
+///Wrapper for a simple poll command.
+struct PollPayload {
+    command: Commands,
+}
+
+impl Default for PollPayload {
+    fn default() -> Self {
+        return PollPayload { command: Commands::SYSTEM_STATUS};
+    }
+}
+
+impl Serialise for PollPayload {
+    fn serialise(self) ->  Vec<u8> {
+        return vec![self.command as u8];
+    }
+}
+
+#[derive(Copy, Clone)]
+struct SystemStatusPayload {
+    switchsetting: SwitchSettingFlags,
+    ledindication: LEDFlags,
+    systemstatus: SystemStatus,
+    cpufirmwareversion: u8,
+    pldfirmwareversion: u8,
+    dspsoftwareversion: u8,
+    dspstatus: Result<i8, DSPError>,
+    numtargetsseen: u8,
+    numtargetsidentified: u8,
+    numtargetsused: u8,
+    rmserror: Pixel32768th,
+}
+
+#[derive(Copy, Clone, Default)]
+struct SystemControlPayload {
+    studioid: u8,
+    smoothing: u8,
+    maxasymmetry: u8,
+    halfboxwidth: u8,
+    blackvidthreshold: u8,
+    whitevidthreshold: u8,
+    blackvidclip: u8,
+    whitevidclip: u8,
+    maxblackpixels: u8,
+    minwhitepixels: u8 
+}
+
+#[derive(Copy, Clone, Default)]
+struct TargetDataPayload {
+    studioid: u8,
+    targetnumber: u16,
+    targetx: i24,
+    targety: i24,
+    targetz: i24,
+    targetflags: i24,
+
+}
+
+#[derive(Copy, Clone, Default)]
+struct ImageDataPayload {
+    targetindex: u8,
+    targetnum: u16,
+    targetx: i24,
+    targety: i24, 
+    xerror: i24,
+    yerror: i24,
+}
+
+#[derive(Copy, Clone, Default)]
+struct EEPROMDataPayload {
+    EEPROMaddress: u16,
+    EEPROMData: [u8; 16],
+}
+
+
+#[derive(Copy, Clone, Default)]
+struct EEPROMDataRequestPayload {
+    EEPROMaddress: u16,
+}
+
+#[derive(Copy, Clone, Default)]
+struct CameraCalibrationPayload {
+    lenscentrex: i24,
+    lenscentrey: i24,
+    lensscalex: i24,
+    lesscaley: i24,
+    lensdistortiona: i24,
+    lensdistortionb: i24,
+    xoffset: i24,
+    yoffset: i24,
+    zoffset: i24
+}
+
+#[derive(Copy, Clone, Default)]
+struct DiagnosticModePayload {
+    diagnosticflag: DiagnosticModes
+}
 
 #[derive(Copy, Clone)]
 #[cfg_attr(test, derive(PartialEq, Debug))]
 ///Plain-Old-Data struct that contains payload information for a POSITION_POLL request.
-/// Note that most fields are 24 bit (as required by the protocol spec) - this will panic if you 
+/// Note that most fields are 24 bit (as required by the protocol spec) - this will panic if you
 /// attempt to place too large or small values into it. Use the `u24::new()` (or `i24::new()`) function to generate values
-/// from literals or primitive integer types. 
+/// from literals or primitive integer types.
+
+//TODO: Compile time checks?
 struct PositionPollPayload {
     pitch: i24,
     yaw: i24,
@@ -22,19 +121,22 @@ struct PositionPollPayload {
     pos_x: i24,
     zoom: u24,
     focus: u24,
-    reserved: u16
-}
-
-trait Serialise {
-    ///Generate an arbitrary array of `u8`s.
-    fn serialise(self) ->  Vec<u8>;
+    userdefined: u16,
 }
 
 impl Default for PositionPollPayload {
     fn default() -> Self {
-        Self { pitch: i24::new(0), yaw: i24::new(0), roll: i24::new(0), 
-            pos_z: i24::new(0), pos_y: i24::new(0), pos_x: i24::new(0), 
-            zoom: u24::new(0), focus: u24::new(0), reserved: 0 }
+        Self {
+            pitch: i24::new(0),
+            yaw: i24::new(0),
+            roll: i24::new(0),
+            pos_z: i24::new(0),
+            pos_y: i24::new(0),
+            pos_x: i24::new(0),
+            zoom: u24::new(0),
+            focus: u24::new(0),
+            userdefined: 0,
+        }
     }
 }
 
@@ -43,8 +145,8 @@ impl Serialise for PositionPollPayload {
     /// Intended as the last step before transmitting over Serial or UDP.    
     fn serialise(self) -> Vec<u8> {
         let mut serial: [u8; 26] = [0; 26];
-        
-        let pitch32 : i32 = self.pitch.into();
+
+        let pitch32: i32 = self.pitch.into();
         let yaw32: i32 = self.yaw.into();
         let roll32: i32 = self.roll.into();
         let pos_z32: i32 = self.pos_z.into();
@@ -53,27 +155,26 @@ impl Serialise for PositionPollPayload {
         let zoom32: u32 = self.zoom.into();
         let focus32: u32 = self.focus.into();
 
-        let i32fields: [i32; 6] = [pitch32, yaw32, roll32, pos_z32, pos_y32, pos_x32]; 
+        let i32fields: [i32; 6] = [pitch32, yaw32, roll32, pos_z32, pos_y32, pos_x32];
         let u32fields: [u32; 2] = [zoom32, focus32];
 
         for (index, field) in i32fields.iter().enumerate() {
             let [__a, b, c, d] = field.to_be_bytes();
-            
-            
-            serial[(index*3) + 0] = b;
-            serial[(index*3) + 1] = c;
-            serial[(index*3) + 2] = d;
-        }
-        
-        for (index, field) in u32fields.iter().enumerate() {
-            let [__a, b, c, d] = field.to_be_bytes();
-            
-            serial[(index*3) + i32fields.len()*3 + 0] = b;
-            serial[(index*3) + i32fields.len()*3 + 1] = c;
-            serial[(index*3) + i32fields.len()*3 + 2] = d;
+
+            serial[(index * 3) + 0] = b;
+            serial[(index * 3) + 1] = c;
+            serial[(index * 3) + 2] = d;
         }
 
-        let [a, b] = self.reserved.to_be_bytes();
+        for (index, field) in u32fields.iter().enumerate() {
+            let [__a, b, c, d] = field.to_be_bytes();
+
+            serial[(index * 3) + i32fields.len() * 3 + 0] = b;
+            serial[(index * 3) + i32fields.len() * 3 + 1] = c;
+            serial[(index * 3) + i32fields.len() * 3 + 2] = d;
+        }
+
+        let [a, b] = self.userdefined.to_be_bytes();
         serial[serial.len() - 2] = a;
         serial[serial.len() - 1] = b;
 
@@ -81,7 +182,6 @@ impl Serialise for PositionPollPayload {
 
         return constserial;
     }
-
 }
 
 #[derive(Copy, Clone)]
@@ -89,87 +189,68 @@ struct Message<T: Serialise + Default> {
     command: Commands,
     cameraid: u8,
     payload: T,
-    checksum: u8
+    checksum: u8,
 }
 
 impl<T: Serialise + Default + Copy + Clone> Serialise for Message<T> {
-
-    
     fn serialise(self) -> Vec<u8> {
-        
         let payloaddata: Vec<u8> = self.payload.serialise();
         let mut output = vec![self.command as u8, self.cameraid];
         output.extend(payloaddata);
         let checksum = generate_checksum(&output);
         output.push(checksum);
-        return output;       
+        return output;
     }
 }
 
-
 impl<T: Serialise + Default> Message<T> {
     pub fn new(command: Commands, cameraid: u8) -> Message<T> {
-        
-        
         let payload: T = match command {
             Commands::STREAM_MODE_START => todo!(),
             Commands::STREAM_MODE_STOP => todo!(),
             Commands::FREEZE_MODE_START => todo!(),
-            Commands::POSITION_POLL => <T as Default>::default(), 
+            Commands::POSITION_POLL => <T as Default>::default(),
             Commands::SYSTEM_STATUS => todo!(),
             Commands::SYSTEM_PARAMS => todo!(),
             Commands::FIRST_TARGET => todo!(),
             Commands::NEXT_TARGET => todo!(),
             Commands::FIRST_IMAGE => todo!(),
             Commands::NEXT_IMAGE => todo!(),
-            _ => todo!()
+            _ => todo!(),
         };
 
-        Message {command: command, cameraid: cameraid, payload: payload, checksum: 0 }
+        Message {
+            command: command,
+            cameraid: cameraid,
+            payload: payload,
+            checksum: 0,
+        }
     }
     //reset this to return a mutable reference.
     ///Gets a copy of the payload struct
     pub fn get_payload(self) -> T {
         return self.payload;
-        
     }
 
     //Sets the payload struct. Note that the new payload must be the same type as the one retrieved from the message.
     pub fn set_payload(&mut self, payload: T) {
         self.payload = payload;
-    } 
-
-
+    }
 }
 
 //pull this out to public function? Doesn't depend on type
 fn generate_checksum(serialised: &[u8]) -> u8 {
-    let mut checksum: u8 = 0x40;
-    for byte in serialised{
-        checksum = (checksum - byte)%255;
+    let mut checksum: u16 = 0x40;
+    for byte in serialised {
+        let upcast = *byte as u16;
+        checksum = (checksum.wrapping_sub(upcast)) % 256;
     }
 
-    return checksum; //spec says 256.. verify.
+    return (checksum % 256).try_into().unwrap(); //spec says 256.. verify.
 }
 
-#[allow(non_camel_case_types)]
-#[derive(Copy, Clone)]
-enum Commands {
-    STREAM_MODE_STOP = 0x00,
-    STREAM_MODE_START = 0x01,
-    FREEZE_MODE_STOP = 0x02,
-    FREEZE_MODE_START = 0x03,
-    POSITION_POLL = 0xD1,
-    SYSTEM_STATUS = 0xD2,
-    SYSTEM_PARAMS = 0xD3,
-    FIRST_TARGET = 0xD4,
-    NEXT_TARGET = 0xD5,
-    FIRST_IMAGE = 0xD6,
-    NEXT_IMAGE = 0xD7,
-    NEXT_EEPROM = 0xD8,
-    CAMERA_CALIBRATION = 0xDA,
-    DIAGNOSTIC_MODE = 0xDB
-}
+
+
 
 #[cfg(test)]
 mod test {
@@ -193,38 +274,45 @@ mod test {
         let _ = x + y;
     }
 
-
     /*
      * This tests that POSITION_POLL payload structs are serialised to u8 arrays correctly:
      * - Values appear in the right places (sequential C-struct order)
      * - Reconstructing the array preserves values.
-    */
+     */
     #[test]
     fn payload_serialise() {
-        let testpitch : i32 = -1000; //signed negative number for i24
-        let testfocus : u32 = 0xAABBCC; //large unsigned number for u24
+        let testpitch: i32 = -1000; //signed negative number for i24
+        let testfocus: u32 = 0xAABBCC; //large unsigned number for u24
 
-        let payload = PositionPollPayload {pitch: i24::new(testpitch), yaw: i24::new(0), roll: i24::new(0), 
-            pos_z: i24::new(0), pos_y: i24::new(0), pos_x: i24::new(0), 
-            zoom: u24::new(0), focus: u24::new(testfocus), reserved: 0};
-        
+        let payload = PositionPollPayload {
+            pitch: i24::new(testpitch),
+            yaw: i24::new(0),
+            roll: i24::new(0),
+            pos_z: i24::new(0),
+            pos_y: i24::new(0),
+            pos_x: i24::new(0),
+            zoom: u24::new(0),
+            focus: u24::new(testfocus),
+            userdefined: 0,
+        };
+
         let serial = payload.serialise();
         println!("{:?}", serial);
 
         let testpitchbytes = testpitch.to_be_bytes();
-        
+
         //byte values in the array at the expected positions match those generated by to_be_bytes()
         assert_eq!(serial[0], testpitchbytes[1]);
         assert_eq!(serial[1], testpitchbytes[2]);
         assert_eq!(serial[2], testpitchbytes[3]);
 
         //reconstructing pitch from array bytes preserves semantics (=-1000)
-        let mut pitchreconstruct: [u8; 4] = [0xFF;4]; //hack sign-extension in. Not in the spirit of testing. Not super important?
+        let mut pitchreconstruct: [u8; 4] = [0xFF; 4]; //hack sign-extension in. Not in the spirit of testing. Not super important?
         let pitchslice = &serial[0..3];
         assert_eq!(pitchslice.len(), 3);
         pitchreconstruct[1..].clone_from_slice(pitchslice);
         assert_eq!(i32::from_be_bytes(pitchreconstruct), testpitch);
-        
+
         let testfocusbytes = testfocus.to_be_bytes();
 
         //byte values in the array at the expected positions match those generated by to_be_bytes()
@@ -233,8 +321,8 @@ mod test {
         assert_eq!(serial[serial.len() - 2 - 1], testfocusbytes[3]);
 
         //reconstructing focus from array values preserves semantics (0xAABBCC)
-        let mut finalvals : [u8; 4] = [0; 4];
-        let slice = &serial[(serial.len() - 2 - 3)..(serial.len()-2)];
+        let mut finalvals: [u8; 4] = [0; 4];
+        let slice = &serial[(serial.len() - 2 - 3)..(serial.len() - 2)];
         assert_eq!(slice.len(), 3);
         finalvals[1..].clone_from_slice(slice);
         assert_eq!(u32::from_be_bytes(finalvals), testfocus);
@@ -242,11 +330,11 @@ mod test {
 
     #[test]
     fn message_new() {
-        let message = Message::<PositionPollPayload>::new(Commands::POSITION_POLL, ALL_CAMERAS);
-        
+        let message = Message::<_>::new(Commands::POSITION_POLL, ALL_CAMERAS);
+
         assert_eq!(message.command as u8, Commands::POSITION_POLL as u8);
         assert_eq!(message.cameraid, ALL_CAMERAS);
-        
+
         let payload: PositionPollPayload = message.get_payload();
         let testpayload: PositionPollPayload = PositionPollPayload::default();
         assert_eq!(payload, testpayload);
@@ -258,7 +346,7 @@ mod test {
         let testzoom = u24::new(25000);
 
         let mut message = Message::<PositionPollPayload>::new(Commands::POSITION_POLL, ALL_CAMERAS);
-        
+
         let mut payload: PositionPollPayload = message.get_payload();
         let testpayload: PositionPollPayload = PositionPollPayload::default();
         assert_eq!(payload, testpayload);
@@ -285,7 +373,8 @@ mod test {
         let testpitch = i24::new(-1000);
         let testzoom = u24::new(25000);
 
-        let mut message: Message<PositionPollPayload> = Message::new(Commands::POSITION_POLL, ALL_CAMERAS);
+        let mut message: Message<PositionPollPayload> =
+            Message::new(Commands::POSITION_POLL, ALL_CAMERAS);
 
         let mut payload = message.get_payload();
         payload.pitch = testpitch;
@@ -294,13 +383,15 @@ mod test {
         message.set_payload(payload);
 
         let serial = message.serialise();
-        println!("{:?}",serial);
-        
+        println!("{:?}", serial);
+
         assert_eq!(serial.len(), 29);
         assert_eq!(serial[0], Commands::POSITION_POLL as u8);
         assert_eq!(serial[1], ALL_CAMERAS);
 
-        assert_eq!(serial[serial.len()-1], generate_checksum(&serial[..serial.len()-1]));
-
+        assert_eq!(
+            serial[serial.len() - 1],
+            generate_checksum(&serial[..serial.len() - 1])
+        ); //this kinda just checks that the checksum exists - ok so long as the unit tests are correct?
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,115 @@
 mod freed;
+
+mod common {
+    use bitflags::bitflags;
+    pub const ALL_CAMERAS: u8 = 0xFF;
+    pub trait Serialise {
+        ///Generate an arbitrary array of `u8`s.
+        fn serialise(self) ->  Vec<u8>;
+    }
+    ///NewType wrapper for RMS error - each unit is 1/32768th of a pixel.
+    #[derive(Copy, Clone)]
+    pub struct Pixel32768th(pub ux::u24);
+    ///Newtype wrapper for 1 unit in camera control structs. Each unit is 1/64th of a pixel.
+    #[derive(Copy, Clone)]
+    #[cfg_attr(test, derive(PartialEq, Debug))]
+    pub struct Millimetre64th(pub ux::i24);
+
+    #[derive(Copy,Clone)]
+    #[cfg_attr(test, derive(PartialEq, Debug))]
+    pub struct Millimetre32768th(pub ux::i24);
+    
+    #[allow(non_camel_case_types)]
+    #[derive(Copy, Clone)]
+    pub enum Commands {
+        STREAM_MODE_STOP = 0x00,
+        STREAM_MODE_START = 0x01,
+        FREEZE_MODE_STOP = 0x02,
+        FREEZE_MODE_START = 0x03,
+        POSITION_POLL = 0xD1,
+        SYSTEM_STATUS = 0xD2,
+        SYSTEM_PARAMS = 0xD3,
+        FIRST_TARGET = 0xD4,
+        NEXT_TARGET = 0xD5,
+        FIRST_IMAGE = 0xD6,
+        NEXT_IMAGE = 0xD7,
+        NEXT_EEPROM = 0xD8,
+        CAMERA_CALIBRATION = 0xDA,
+        DIAGNOSTIC_MODE = 0xDB,
+    }
+
+    #[allow(non_camel_case_types)]
+    #[derive(Copy, Clone)]
+    pub enum SystemStatus {
+        SYSTEM_NORMAL = 0,
+        PROCESSOR_RESET = 1,
+        SERIAL_ERROR = 2,
+        VBLANK_FAIL = 3,
+        XILINX_FAIL = 4,
+        I2C_FAIL = 5,
+        EEPROM_FAIL = 6,
+        DSP_ACKNOWLEDGE_FAIL = 7,
+        DSP_ACCEPT_FAIL = 8,
+        DSP_PROVIDE_FAIL = 9,
+        DSP_EXCEPTION_ERROR = 10,
+        I2C_NOREPLY_FAIL = 91,
+        I2C_BUSERROR_FAIL = 93,
+        I2C_ACK_FAIL = 94,
+        I2C_UNDEFINED_STATE = 95,
+        I2C_OVERFLOW = 96
+
+    }
+    
+    bitflags! { 
+        #[derive(Copy, Clone)]
+        pub struct LEDFlags: u8 {
+            const VIDEO_PRESENT   = 0b00000001;
+            const VIDEO_OK        = 0b00000010;
+            const SERIAL_PRESENT  = 0b00000100;
+            const DATA_FREEZE     = 0b00001000;
+            const TOO_FEW_TARGETS = 0b00010000;
+            const RMS_ERROR_HIGH  = 0b00100000;
+            const DSP_ALERT       = 0b01000000;
+            const FAULT           = 0b10000000;
+
+        }
+    }
+
+    bitflags! {
+        #[derive(Copy, Clone)]
+        pub struct SwitchSettingFlags: u8 {
+            const S5_HEX_00   = 0b00000001;
+            const S5_HEX_01   = 0b00000010;
+            const S5_HEX_02   = 0b00000100;
+            const S5_HEX_03   = 0b00001000;
+            const S2_LEFT     = 0b00000000;
+            const S2_RIGHT    = 0b00000000;
+            const S4_CLOSED   = 0b00000000;
+            const IS_S3_RIGHT = 0b10000000;
+        }
+    }
+
+    #[allow(non_camel_case_types)]
+    #[derive(Copy, Clone)]
+    pub enum DSPError {
+        TOO_FEW_TARGETS = -1,
+        ITERATION_CONVERGE_FAIL = -2,
+        DSP_RESET = -3,
+        INTERNAL_ERROR = -4,
+    }
+    
+    #[derive(Copy, Clone)]
+    pub enum DSPStatus {
+        Error(DSPError),
+        Iterations(i8)
+    }
+
+    #[derive(Copy, Clone, Default)]
+
+    pub enum DiagnosticModes {
+       #[default] NORMAL_OPERATION = 0x00,
+        VIDEO_DATA_0x55 = 0x40,
+        VIDEO_DATA_0xAA = 0x80,
+        VIDEO_DATA_TEST = 0xC0,
+    }
+}
